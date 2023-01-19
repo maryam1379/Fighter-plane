29fba211ef0042795a9c0bf2a30de01b44b1f852
maryam1379 <s.smr1479@gmail.com>
2023-01-19 08:45:26 +0330

    Movement of planes and their collision settings and adding text

---
+++ Assets/Script/Plane_Spawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane_Spawner : MonoBehaviour
+{
+    public GameObject[] plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+            StartCoroutine(SpawnPlanes());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Planes()
+    {
+            int rand = Random.Range(0,plane.Length);
+            float randXPos= Random.Range(-1.92f,1.92f);
+            Instantiate(plane[rand], new Vector3(randXPos, transform.position.y,transform.position.z) ,Quaternion.Euler(0,0,0));
+        
+    }
+
+    IEnumerator SpawnPlanes() {
+       while(true){
+             yield return new WaitForSeconds(4);
+               Planes();
+       }
+       
+    }
+}
